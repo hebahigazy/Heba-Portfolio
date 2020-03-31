75b7cf9c4f255432a34c7e4b04be8c463c11015a
hebahigazy <hebahigazy@gmail.com>
2020-03-30 20:34:13 -0400

    Updates

---
+++ precache-manifest.7ac80988ba926b32fb597e3f614e299e.js
@@ -0,0 +1,174 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f225d6b02725af99fa8fd62ee5da4624",
+    "url": "/Heba-Portfolio/index.html"
+  },
+  {
+    "revision": "0577d2207a7b492ca722",
+    "url": "/Heba-Portfolio/static/css/main.7828ff43.chunk.css"
+  },
+  {
+    "revision": "63aac7c3425acdf40565",
+    "url": "/Heba-Portfolio/static/js/2.7dd8952a.chunk.js"
+  },
+  {
+    "revision": "0577d2207a7b492ca722",
+    "url": "/Heba-Portfolio/static/js/main.50a1254e.chunk.js"
+  },
+  {
+    "revision": "787637f6417fa4c5a4b0",
+    "url": "/Heba-Portfolio/static/js/runtime~main.a3735ad6.js"
+  },
+  {
+    "revision": "ddb770ad774684a2a266e40ff1ed4092",
+    "url": "/Heba-Portfolio/static/media/anim1.ddb770ad.gif"
+  },
+  {
+    "revision": "78b623c1a28a86ab80fe148e6423a33e",
+    "url": "/Heba-Portfolio/static/media/anim2.78b623c1.gif"
+  },
+  {
+    "revision": "8fa9a845f7bacf32c1a3871f7c1176e5",
+    "url": "/Heba-Portfolio/static/media/anim3.8fa9a845.gif"
+  },
+  {
+    "revision": "17d1f3b9c6228a1f2cf9dcc148edcf88",
+    "url": "/Heba-Portfolio/static/media/anim4.17d1f3b9.gif"
+  },
+  {
+    "revision": "b4c655668e9e36d5c88aadffd97bea51",
+    "url": "/Heba-Portfolio/static/media/anim5.b4c65566.gif"
+  },
+  {
+    "revision": "f1d71f777331fd7e3de116edf4ee3b67",
+    "url": "/Heba-Portfolio/static/media/avatar.f1d71f77.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/Heba-Portfolio/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "e4b3cdc584ff08f6122b069fc66cf8ff",
+    "url": "/Heba-Portfolio/static/media/bg7.e4b3cdc5.jpg"
+  },
+  {
+    "revision": "1749a4888bc2a53bdf75e88883dc52fa",
+    "url": "/Heba-Portfolio/static/media/bg8.1749a488.jpg"
+  },
+  {
+    "revision": "56633ed3f62f39d71f571374a6409e65",
+    "url": "/Heba-Portfolio/static/media/christian.56633ed3.jpg"
+  },
+  {
+    "revision": "c058841b1dd64e5f79c6348b24cfb78d",
+    "url": "/Heba-Portfolio/static/media/clem-onojegaw.c058841b.jpg"
+  },
+  {
+    "revision": "216ef03c54bc13771c5e1b8d8f8d5926",
+    "url": "/Heba-Portfolio/static/media/clem-onojeghuo.216ef03c.jpg"
+  },
+  {
+    "revision": "9813593cc577a319a2201342ef0fb237",
+    "url": "/Heba-Portfolio/static/media/cynthia-del-rio.9813593c.jpg"
+  },
+  {
+    "revision": "457ac45fd3139ce16d63bd2291a10ae3",
+    "url": "/Heba-Portfolio/static/media/gd1.457ac45f.jpg"
+  },
+  {
+    "revision": "1d480ec1f5554b047524298decdb9ec5",
+    "url": "/Heba-Portfolio/static/media/gd2.1d480ec1.jpg"
+  },
+  {
+    "revision": "bd165d8c912c78949eed4d97479e1e7b",
+    "url": "/Heba-Portfolio/static/media/gd4.bd165d8c.jpg"
+  },
+  {
+    "revision": "5abd875b433133a9c0c232d2a439e259",
+    "url": "/Heba-Portfolio/static/media/gd5.5abd875b.jpg"
+  },
+  {
+    "revision": "e61fbbb7d15d5748dc9832edc0d1743b",
+    "url": "/Heba-Portfolio/static/media/gd6.e61fbbb7.jpg"
+  },
+  {
+    "revision": "e0508cc923eb0b5e68ca6783c53d0f1d",
+    "url": "/Heba-Portfolio/static/media/kendall.e0508cc9.jpg"
+  },
+  {
+    "revision": "df8fd3efcd662b64b44de07f351c838b",
+    "url": "/Heba-Portfolio/static/media/landing-bg.df8fd3ef.jpg"
+  },
+  {
+    "revision": "ace16d9c32cc052cfc029177385bc65b",
+    "url": "/Heba-Portfolio/static/media/logo.ace16d9c.svg"
+  },
+  {
+    "revision": "4575c40bfb8dec6713d2da51c4c4066e",
+    "url": "/Heba-Portfolio/static/media/mariya-georgieva.4575c40b.jpg"
+  },
+  {
+    "revision": "4112cbc1477d9e149033c5df66087e16",
+    "url": "/Heba-Portfolio/static/media/olu-eletu.4112cbc1.jpg"
+  },
+  {
+    "revision": "966d71af5d10b57cd9af78df4eb76c00",
+    "url": "/Heba-Portfolio/static/media/studio-1.966d71af.jpg"
+  },
+  {
+    "revision": "76e2987ed95634136dd22d4d9e1009a7",
+    "url": "/Heba-Portfolio/static/media/studio-2.76e2987e.jpg"
+  },
+  {
+    "revision": "1d5451ced89eabb55683e27e070bdb60",
+    "url": "/Heba-Portfolio/static/media/studio-3.1d5451ce.jpg"
+  },
+  {
+    "revision": "e064d0908dbd53b55f8980c02b3748bb",
+    "url": "/Heba-Portfolio/static/media/studio-4.e064d090.jpg"
+  },
+  {
+    "revision": "ef5c30ea69b7ad740ee6221782c73741",
+    "url": "/Heba-Portfolio/static/media/studio-5.ef5c30ea.jpg"
+  },
+  {
+    "revision": "18b113f989a6c7712377acf6aadbba46",
+    "url": "/Heba-Portfolio/static/media/ux1.18b113f9.jpg"
+  },
+  {
+    "revision": "043c1c49afa43338e8fc93d5e5648836",
+    "url": "/Heba-Portfolio/static/media/ux2.043c1c49.jpg"
+  },
+  {
+    "revision": "088790f031e4928b7e08a4782e718629",
+    "url": "/Heba-Portfolio/static/media/ux3.088790f0.jpg"
+  },
+  {
+    "revision": "99f513d8a8265742622c0dfed6a5eed3",
+    "url": "/Heba-Portfolio/static/media/ux4.99f513d8.jpg"
+  },
+  {
+    "revision": "cf3c6813749c16ab2b5f0fb8478aa3e9",
+    "url": "/Heba-Portfolio/static/media/ux5.cf3c6813.jpg"
+  },
+  {
+    "revision": "ce38f5a8958709cf74efbb18eb549768",
+    "url": "/Heba-Portfolio/static/media/web1.ce38f5a8.jpg"
+  },
+  {
+    "revision": "c82ffda1ff42301d6652527561a9a655",
+    "url": "/Heba-Portfolio/static/media/web2.c82ffda1.jpg"
+  },
+  {
+    "revision": "d128c28ed7f076c04993da7aea664a5a",
+    "url": "/Heba-Portfolio/static/media/web3.d128c28e.jpg"
+  },
+  {
+    "revision": "98ef622bd7a28be6ab685bd39e791be8",
+    "url": "/Heba-Portfolio/static/media/web4.98ef622b.jpg"
+  },
+  {
+    "revision": "dfbf2ee04e5c9634898296770c0f969c",
+    "url": "/Heba-Portfolio/static/media/web5.dfbf2ee0.jpg"
+  }
+]);
